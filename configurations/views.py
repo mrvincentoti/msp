9b3c26434e3c8d15456145a314fc804135eb41fe
vincentoti <vincent.oti@layer3.com.ng>
2022-10-18 13:16:07 +0100

    Created configuration app

---
+++ configurations/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def configurations(request):
+    return render(request, 'configurations/configurations.html', {})
--- mspapp/settings.py
+++ mspapp/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mspapp'
+    'mspapp',
+    'configurations'
 ]
 
 MIDDLEWARE = [
